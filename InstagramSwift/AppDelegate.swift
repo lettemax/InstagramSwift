99c5e41a2411ccf1e7b8d03f79f4f4108db79998
lettemax <maxlettenberger@gmail.com>
2015-06-09 14:13:10 -0700

    first commit, has signup/login

--- InstagramSwift/AppDelegate.swift
+++ InstagramSwift/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+import Parse
+import Bolts
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+
+        // Initialize Parse.
+        Parse.setApplicationId("oQfvAW1ZGl6mlzmUgTyHQlQPucQBhaDOB3BN9V49",
+            clientKey: "daXOnTnQcP9b1n6ZvZ8dffAAXOcy5fCoyfaZkpJa")
+
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        // ...
+
         return true
     }
 
